6aaf6fbab57bd4b5143f6ab673ca969ca36e4a3f
daniel.martinez <daniel.martinez@tutenlabs.com>
2023-04-05 22:12:36 -0400

    remove unset var

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Home from './pages/home';
 import Destination from './pages/destination';
